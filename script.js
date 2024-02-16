da70ffc9a50db8ecb2043cbdb5c09e392d4a81af
Mateus David de Souza <73516308+mtds20@users.noreply.github.com>
2024-02-16 20:25:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+/* 
+   Welcome to the Magic 8-ball Project.  Complete the 
+   tasks outlined by the comments in this file.
+
+   The following functions are available to you 
+   (you can just call them):
+
+   // This will hide the '8' (if shown) and 
+   // show the triangle (if hidden).  It will
+   // set the text on the triangle to the
+   // string 'answerText'.
+   function makeAnswerAppear(answerText)
+*/
+
+/* Returns a random integer in the range 'min' through 'max' inclusive. 
+
+   This can be taken directly from MDN documentation: 
+     https://tinyurl.com/3jkxa8h3
+
+*/
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/* Write a function that handles the magic 8-ball being clicked. Here are the steps:
+
+   - Create an array containing at least 5 possible answers as strings.
+   - Generate a random array index by calling 
+       getRandomIntInclusive function.
+   - Call 'makeAnswerAppear' using the random
+       answer you selected.
+   - (Level-up) Prevent your code from selecting the 
+       same answer multiple times in a row 
+       (loops could be required).
+*/
+
+let previousAnswerSelected = "Hey";
+
+function answerQuestion() {
+  let answers = ["Try again", "I do not think so", "Probably not", "Yes", "No", "Maybe"];
+  let answerSelected = answers[getRandomIntInclusive(0, answers.length - 1)];
+  
+  while (answerSelected === previousAnswerSelected) {
+    answerSelected = answers[getRandomIntInclusive(0, answers.length - 1)];
+  }
+  
+  previousAnswerSelected = answerSelected;
+  makeAnswerAppear(answerSelected);
+
+}
